8aa3a68dc80a6bb4b7f5f0e0f16ea58084136a4d
anik112 <paulanik112@gmail.com>
2020-12-22 17:19:19 +0600

    Add new work.

--- Student-Management-System/StudentManagementSystem.cpp
+++ Student-Management-System/StudentManagementSystem.cpp
@@ -2,10 +2,24 @@
 #include<stdio.h>
 #include<windows.h>
 #include<ctype.h>
+#include<fstream>
+#include<stdlib.h>
 
 using namespace std;
 
-class StudentDao{
+const string semesters[]= {"1st","2nd","3rd"};
+const string subjects[]= {"CSE","EEE","MAT"};
+const int lenOfSemesters = sizeof(semesters)/sizeof(semesters[0]);
+const int lenOfSubjects=sizeof(subjects)/sizeof(subjects[0]);
+
+#define NC "\e[0m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define CYN "\e[0;36m"
+#define REDB "\e[41m"
+
+class StudentDao
+{
 public:
     int stdId;
     string stdName;
@@ -21,7 +35,13 @@ public:
     string stdSemester;
 };
 
-void gotoxy(int x,int y){
+void gotoxy(int x,int y)
+{
+    /*
+    Console width: 100;
+    Console hight: 30;
+    */
+
     COORD coord;
     coord.X=x;
     coord.Y=y;
@@ -29,45 +49,101 @@ void gotoxy(int x,int y){
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
-void headerPart(){
-    gotoxy(5,4);
-    cout<<"=============================================================================";
+void headerPart()
+{
+    gotoxy(10,1);
+    for(int i=0;i<80;i++){
+        cout<<(char)220;
+    }
+    gotoxy(10,2);
+    for(int i=0;i<80;i++){
+        cout<<(char)219;
+    }
 }
 
-void footerPart(){
-    gotoxy(5,20);
-    cout<<"=============================================================================";
+void rightSide(){
+    for(int i=2;i<27;i++){
+        gotoxy(10,i);
+        cout<<(char)219;
+    }
 }
 
-bool login(){
+void leftSide(){
+    for(int i=2;i<27;i++){
+        gotoxy(89,i);
+        cout<<(char)219;
+    }
+}
+
+void footerPart()
+{
+    gotoxy(10,27);
+    for(int i=0;i<80;i++){
+        cout<<(char)219;
+    }
+    gotoxy(10,28);
+    for(int i=0;i<80;i++){
+        cout<<(char)196;
+    }
+}
+
+bool login()
+{
     int pass;
     headerPart();
-    gotoxy(25,7); cout<<"########## Login ##########";
-    gotoxy(25,9); cout<<"Password: "; cin>>pass;
-    gotoxy(25,11); cout<<"##########################";
+    leftSide();
+
+    gotoxy(35,9);
+    cout<<"########## Login ##########";
+    gotoxy(35,11);
+    cout<<"Password: ";
+    gotoxy(35,13);
+    cout<<"###########################";
+    rightSide();
     footerPart();
 
-    if(pass==123){
+    gotoxy(45,11);
+    cin>>pass;
+    if(pass==123)
+    {
         system("cls");
         return true;
-    }else{
+    }
+    else
+    {
         return false;
     }
 }
 
-void addStudent(){
-    FILE *sutd;
-    sutd=fopen("student_info.txt","a");
-    string is="anik paul";
-    char buffer[] = { 'x' , 'y' , 'z' };
-    fwrite(&buffer,sizeof(buffer),1,sutd);
-    fclose(sutd);
+void addStudent()
+{
+    headerPart();
+    leftSide();
+
+    StudentDao student;
+    ofstream file ("student_info.txt",ofstream::app);
+    gotoxy(15,5); cout<<(char)219<<" Add New Student "<<(char)219;
+    gotoxy(15,7); cout<<"Student ID: ";
+    gotoxy(15,8); cout<<"Student Name: ";
+    gotoxy(15,9); cout<<"Father Name: ";
+    gotoxy(15,10); cout<<"Mother Name: ";
+    rightSide();
+    footerPart();
+
+
+    gotoxy(35,7); cin>>student.stdId;
+    gotoxy(35,8); cin>>student.stdName;
+    gotoxy(35,9); cin>>student.stdFatherName;
+    gotoxy(35,10); cin>>student.stdMotherName;
+
+    file <<student.stdId<<"#"<<student.stdName<<":";
+    file.close();
+
 }
 
 
-int main(){
-    string semesters[]={"1st","2nd","3rd"};
-    string subjects[]={"CSE","EEE","MAT"};
+int main()
+{
 
     if(login()){
         addStudent();
